4e2e61858ecabd97115558381584cba2e7441045
Renita Kurian <rrenita1206@gmail.com>
2022-11-04 22:00:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,22 +1,33 @@
 import pandas as pd
 
-data = pd.read_csv("./music.csv")
-name = pd.read_csv("./name.csv")
+music = pd.read_csv("music.csv")
+music = music.sample(frac = 1)
+#music.head()
 
-df = pd.DataFrame(columns = ['id', 'label', 'name', '_start', '_end', '_type', '_rating'])
-unique_genre = data["genre"].unique()
+name = pd.read_csv("names.csv")
 
+unique_genres = music["genre"].unique().tolist()
+#print(unique_genres)
+tracks = music["track_name"].unique().tolist()[0:994]
 
-for i in range(1,8):
-	df.loc[i] = [i - 1, ":genre", unique_genre[i - 1], '', '', '', '']
+df = pd.DataFrame(columns = ['id', 'label', 'name', '_start', '_end', '_type', 'rating'])
 
-for i in range(8, 1002):
-	df.loc[i] = [i - 1, ":track", data["track_name"][i - 8], '', '', '', '']
-
-for i in range(1002, 1302):
-	df.loc[i] = [i - 1, ":user", name["name"][i - 1000], '', '', '', '']
-
+for i in range(0,7):
+    df.loc[i] = [i, ":genre", unique_genres[i], '', '', '', '']
 
 
-print(df)
+for i in range(7, 1001):
+	df.loc[i] = [i, ":track", tracks[i-7], '', '', '', '']
+
+for i in range(1001, 1301):
+    df.loc[i] = [i, ":user", name["name"][i - 1001], '', '', '', '']
+
+row_no = 1001
+
+
+for i in tracks:
+    df.loc[row_no] = [ '', '', '', df.loc[df['name'] == i]['id'].iat[0], unique_genres.index(music.loc[music['track_name'] == i]['genre'].iat[0]), 'BELONGS_TO', '']
+    row_no+=1
+
+df.to_csv('graph_dataset.csv')
 
