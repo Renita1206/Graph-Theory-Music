e0ed52c52b17417bc47d673d822a29dc296caaf1
Uchiha268 <79242362+Uchiha268@users.noreply.github.com>
2022-11-05 02:47:00 +0530

    Update script.py
    Added LISTENS_TO relation

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 
 music = pd.read_csv("music.csv")
 music = music.sample(frac = 1)
@@ -9,6 +10,7 @@ name = pd.read_csv("names.csv")
 unique_genres = music["genre"].unique().tolist()
 #print(unique_genres)
 tracks = music["track_name"].unique().tolist()[0:994]
+users = name["name"].tolist()[0:299]
 
 df = pd.DataFrame(columns = ['id', 'label', 'name', '_start', '_end', '_type', 'rating'])
 
@@ -22,12 +24,16 @@ for i in range(7, 1001):
 for i in range(1001, 1301):
     df.loc[i] = [i, ":user", name["name"][i - 1001], '', '', '', '']
 
-row_no = 1001
+row_no = 1301
 
 
 for i in tracks:
     df.loc[row_no] = [ '', '', '', df.loc[df['name'] == i]['id'].iat[0], unique_genres.index(music.loc[music['track_name'] == i]['genre'].iat[0]), 'BELONGS_TO', '']
     row_no+=1
 
-df.to_csv('graph_dataset.csv')
+for i in range(0, len(tracks)):
+	k = random.randrange(300)
+	df.loc[row_no] = [ '', '', '', k + 1001, i + 7, 'LISTENS_TO', '']
+	row_no += 1
 
+df.to_csv('graph_dataset.csv')
